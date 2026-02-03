2dad8478e25ee8ae091088151ba7a04613a5bccf
Justin Galvez <galvezjustin@hotmail.com>
2026-02-02 20:54:40 -0800

    Added middleware and error handling examples

--- src/app.js
+++ src/app.js
@@ -1,11 +1,39 @@
 import express from 'express';
 import Database from 'better-sqlite3';
 
-const db = new Database('favorites.db');
+const db = new Database('favorite.db');
 const app = express();
 const port = 3000;
 
+// Middleware
+// Used to do mini service before hitting server e.g. logging/authentication/etc.
+// needs to invoke next to continue to the next thing
+app.use((req, res, next) => {
+  console.log('hit 1');
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('hit 2');
+  next();
+});
+
+// Add midware to another method
+
+// app.get('/favorites', (req, res, next) => {
+//   console.log('Pre processing...');
+//   next();
+// });
+
+//OR
+
+// const authenticate = (req, res, next) => {
+//   console.log('Pre processing...');
+//   next();
+// };
+
 // Handler for GET method
+// Add authenticate in parameter to use in that way
 app.get('/favorites', (req, res) => {
   let query = 'SELECT * FROM favorites';
   const sort = req.query.sort;
@@ -24,13 +52,14 @@ app.get('/favorites', (req, res) => {
 });
 
 // GET method with id
-app.get('/favorites/:id', (req, res) => {
+app.get('/favorites/:id', (req, res, next) => {
+  // try {
   // Grab id from url request
   const id = parseInt(req.params.id);
 
   // Check if id matches any id in favorites array (iterates through all of them to find the first match)
   // Return favorites object if found to var favorite
-  const favorite = favorites.find((fav) => fav.id == id);
+  const favorite = db.prepare('SELECT * FROM favorites WHERE id = ?').get(id);
 
   if (!favorite) {
     // return is needed here to fix CANNOT SET HEADERS AFTER THEY ARE SENT TO THE CLIENT issue
@@ -41,6 +70,30 @@ app.get('/favorites/:id', (req, res) => {
 
   // Like to enclose this with brackets to add a favorite id in front of the object
   res.json({ favorite });
+
+  // Part of try catch block - useful if you want middle ware in this function as well for whatever reason
+
+  // } catch (err) {
+  //   console.log('CUSTOM TO THIS ROUTE');
+  //   next(err);
+  // }
+});
+
+app.use((err, req, res, next) => {
+  // console.log(err);
+
+  // Can check for specific error
+
+  if (err.name === 'SqliteError') {
+    console.log('db error hit!');
+  }
+
+  // Can end middleware pipeline using something like
+
+  // res.status(500).json({ error: 'Something went wrong, try again later' });
+
+  // Or continue using
+  next(err);
 });
 
 app.listen(port, () => {
